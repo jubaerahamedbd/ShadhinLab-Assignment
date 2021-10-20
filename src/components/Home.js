6ba72edbc804a89ff92b486767c2de069ef6f131
jubaerahamedbd <jubaerahamed@gmail.com>
2021-10-20 21:16:21 +0600

    tab

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,30 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Button } from 'reactstrap'
 import { Link } from "react-router-dom";
-import { Country, State, City }  from 'country-state-city';
+import { Country, State, City } from 'country-state-city';
+import AddUser from "./Users/AddUser";
 
 const Home = () => {
+    
     const [users, setUsers] = useState([])
-
+    const [openModal, setOpenModal] = useState(false)
     useEffect(() => {
         loadUsers()
     }, [])
-    console.log(Country.getAllCountries())
-    console.log(State.getAllStates())
+    
     const loadUsers = async () => {
         const result = await axios.get("https://60f2479f6d44f300177885e6.mockapi.io/users")
-        setUsers(result.data.reverse())
+        //setUsers(result.data.reverse())
+
+        setUsers(
+            result.data.filter((data) => {
+                if (data.user_type === 'admin') return true;
+            })
+        )
     }
+
+
+
     const deleteUser = async id => {
         await axios.delete(`https://60f2479f6d44f300177885e6.mockapi.io/users/${id}`)
         loadUsers();
@@ -23,49 +33,89 @@ const Home = () => {
 
     return (
         <div className="container">
-        <div className="top">
-        <h3 >Users List</h3>
-                <Link className="btn btn-outline-dark col-md-3 " id="addUser" to="/user/add">Add User</Link>
-        </div>
-                
-
-
+            <div className="top">
+                <h3 >Users List</h3>
+                <Link className="btn btn-outline-dark col-md-3 " id="addUser" onClick={() => setOpenModal(true)} to="/">Add User</Link>
+            </div>
             <hr />
-
-            <table class="table table-striped text-center shadow">
-                <thead>
-                    <tr>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Division</th>
-                        <th scope="col">District</th>
-                        <th scope="col">User Type</th>
-                        <th scope="col">Details View</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-
-                        users.map((user, index) => (
-
+           
+            <div className="mytabs">
+                <input type="radio" id="admin" name="mytabs" checked="checked" />
+                <label for="admin">Admin View</label>
+                <div className="tab">
+                    <table class="table table-striped text-center shadow">
+                        <thead>
                             <tr>
-                                <td>{user.first_name}</td>
-                                <td>{user.last_name} </td>
-                                <td>{user.division}</td>
-                                <td>{user.district}</td>
-                                <td>{user.user_type}</td>
-                                <td><Link className="btn btn-primary mx-2" to={`/user/${user.id}`}>Details</Link>
-                                    <Link className="btn btn-danger mx-2" onClick={() => deleteUser(user.id)}>Delete</Link>
-                                </td>
-
+                                <th scope="col">First Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col">Division</th>
+                                <th scope="col">District</th>
+                                <th scope="col">User Type</th>
+                                <th scope="col">Details View</th>
                             </tr>
+                        </thead>
+                        <tbody>
+                            {
 
-                        ))
-                    }
+                                users.map((user, index) => (
 
-                </tbody>
-            </table>
+                                    <tr>
+                                        <td>{user.first_name}</td>
+                                        <td>{user.last_name} </td>
+                                        <td>{user.division}</td>
+                                        <td>{user.district}</td>
+                                        <td>{user.user_type}</td>
+                                        <td><Link className="btn btn-primary mx-2" to={`/user/${user.id}`}>Details</Link>
+                                            <Link className="btn btn-danger mx-2" onClick={() => deleteUser(user.id)}>Delete</Link>
+                                        </td>
 
+                                    </tr>
+
+                                ))
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+                <input type="radio" id="employee" name="mytabs" checked="checked" />
+                <label for="employee" >Employee View</label>
+                <div className="tab">
+                    <table class="table table-striped text-center shadow">
+                        <thead>
+                            <tr>
+                                <th scope="col">First Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col">Division</th>
+                                <th scope="col">District</th>
+                                <th scope="col">User Type</th>
+                                <th scope="col">Details View</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+
+                                users.map((user, index) => (
+
+                                    <tr>
+                                        <td>{user.first_name}</td>
+                                        <td>{user.last_name} </td>
+                                        <td>{user.division}</td>
+                                        <td>{user.district}</td>
+                                        <td>{user.user_type}</td>
+                                        <td><Link className="btn btn-primary mx-2" to={`/user/${user.id}`}>Details</Link>
+                                            <Link className="btn btn-danger mx-2" onClick={() => deleteUser(user.id)}>Delete</Link>
+                                        </td>
+
+                                    </tr>
+
+                                ))
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            {openModal && <AddUser closeModal={setOpenModal} />}
         </div>
     )
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Country, State, City } from "country-state-city";
-const AddUser = () => {
+const AddUser = ({ closeModal }) => {
     let history = useHistory()
     const [user, setUser] = useState({
         first_name: "",
@@ -16,132 +16,136 @@ const AddUser = () => {
     const { first_name, last_name, user_type, division, district } = user
     const [divisions, setDivisions] = useState(
         State.getStatesOfCountry("BD").filter((dataItem) => {
-          if (dataItem.name.includes("Division")) return true;
+            if (dataItem.name.includes("Division")) return true;
         })
-      );
-      const [districts, setDistricts] = useState([]);
-    
-      const OnInputChange = (e) => {
+    );
+    const [districts, setDistricts] = useState([]);
+
+    const OnInputChange = (e) => {
         setUser({
-          ...user,
-          [e.target.name]:
-            e.target.name === "Division"
-              ? e.target.value.split("-")[0]
-              : e.target.value,
+            ...user,
+            [e.target.name]:
+                e.target.name === "Division"
+                    ? e.target.value.split("-")[0]
+                    : e.target.value,
         });
-      };
+    };
     const onSubmit = async e => {
         e.preventDefault()
         await axios.post("https://60f2479f6d44f300177885e6.mockapi.io/users", user)
         history.push("/")
+        
+        closeModal(false)
     }
     useEffect(() => {
-        console.log(State.getStatesOfCountry("BD"));
-      }, []);
+        
+    }, []);
     return (
-        <div className="container">
-            <div className="col-md-8 updateUser">
-                <div className="top">
-                    <h2>Add User</h2>
-                    <Link className="btn btn-outline-dark col-md-6" id="addUser" to="/">Back To User List</Link>
+        <div className="AddUserLayout">
+            <div className="container">
+                <div className="col-md-8 updateUser">
+                    <div className="top">
+                        <h2>Add User</h2>
+                        <Link className="btn btn-outline-dark col-md-6" id="addUser" onClick={() => closeModal(false)} to="/">Back To User List</Link>
+                    </div>
+                    <hr />
+                    <form onSubmit={e => onSubmit(e)}>
+                        <div class="form-group row my-2">
+
+                            <label class="col-sm-3 col-form-label col-form-label-sm">First Name:</label>
+
+                            <div class="col-sm-9" >
+                                <input
+                                    class="col-sm-9"
+                                    type="text"
+                                    class="form-control"
+                                    name="first_name"
+                                    value={first_name}
+                                    placeholder="Enter First Name"
+                                    onChange={e => OnInputChange(e)}
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group row my-2">
+
+                            <label class="col-sm-3 col-form-label col-form-label-sm">Last Name</label>
+                            <div class="col-sm-9" >
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    name="last_name"
+                                    value={last_name}
+                                    placeholder="Enter Last Name"
+                                    onChange={e => OnInputChange(e)}
+                                />
+                            </div>
+
+                        </div>
+                        <div class="form-group row my-2">
+                            <label class="col-sm-3 col-form-label col-form-label-sm">
+                                Division
+                            </label>
+                            <div class="col-sm-9">
+                                <select
+                                    onChange={(e) => {
+                                        OnInputChange(e);
+                                        setDistricts(
+                                            City.getCitiesOfState("BD", e.target.value.split("-")[1])
+                                        );
+                                    }}
+                                    class="form-control form-control-sm"
+                                    name="division"
+                                >
+                                    {divisions.map((dataItem) => {
+                                        return (
+                                            <option value={dataItem.name + "-" + dataItem.isoCode}>
+                                                {dataItem.name}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row my-2">
+                            <label class="col-sm-3 col-form-label col-form-label-sm">
+                                District
+                            </label>
+                            <div class="col-sm-9">
+                                <select
+                                    onChange={(e) => {
+                                        OnInputChange(e);
+                                    }}
+                                    class="form-control form-control-sm"
+                                    name="district"
+                                >
+                                    {districts.map((dataItem) => {
+                                        return <option value={dataItem.name}>{dataItem.name}</option>;
+                                    })}
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row my-2">
+                            <label class="col-sm-3 col-form-label col-form-label-sm"    >User Type</label>
+                            <div class="col-sm-9">
+
+                                <select
+                                    onChange={(e) => OnInputChange(e)}
+                                    class="form-control form-control-sm"
+                                    name="user_type"
+                                >
+                                    <option value={"admin"}>admin</option>
+                                    <option value={"employee"}>employee</option>
+                                </select>
+                            </div>
+
+                        </div>
+
+                        <button type="submit"  class="btn btn-primary updatebtn">Add User</button>
+                    </form>
                 </div>
-                <hr />
-                <form onSubmit={e => onSubmit(e)}>
-                    <div class="form-group row my-2">
 
-                        <label class="col-sm-3 col-form-label col-form-label-sm">First Name:</label>
-
-                        <div class="col-sm-9" >
-                            <input
-                                class="col-sm-9"
-                                type="text"
-                                class="form-control"
-                                name="first_name"
-                                value={first_name}
-                                placeholder="Enter First Name"
-                                onChange={e => OnInputChange(e)}
-                            />
-                        </div>
-                    </div>
-                    <div class="form-group row my-2">
-
-                        <label class="col-sm-3 col-form-label col-form-label-sm">Last Name</label>
-                        <div class="col-sm-9" >
-                            <input
-                                type="text"
-                                class="form-control"
-                                name="last_name"
-                                value={last_name}
-                                placeholder="Enter Last Name"
-                                onChange={e => OnInputChange(e)}
-                            />
-                        </div>
-
-                    </div>
-                    <div class="form-group row my-2">
-            <label class="col-sm-3 col-form-label col-form-label-sm">
-              Division
-            </label>
-            <div class="col-sm-9">
-              <select
-                onChange={(e) => {
-                  OnInputChange(e);
-                  setDistricts(
-                    City.getCitiesOfState("BD", e.target.value.split("-")[1])
-                  );
-                }}
-                class="form-control form-control-sm"
-                name="division"
-              >
-                {divisions.map((dataItem) => {
-                  return (
-                    <option value={dataItem.name + "-" + dataItem.isoCode}>
-                      {dataItem.name}
-                    </option>
-                  );
-                })}
-              </select>
             </div>
-          </div>
-
-          <div class="form-group row my-2">
-            <label class="col-sm-3 col-form-label col-form-label-sm">
-              District
-            </label>
-            <div class="col-sm-9">
-              <select
-                onChange={(e) => {
-                  OnInputChange(e);
-                }}
-                class="form-control form-control-sm"
-                name="district"
-              >
-                {districts.map((dataItem) => {
-                  return <option value={dataItem.name}>{dataItem.name}</option>;
-                })}
-              </select>
-            </div>
-          </div>
-                    <div class="form-group row my-2">
-                        <label class="col-sm-3 col-form-label col-form-label-sm"    >User Type</label>
-                        <div class="col-sm-9">
-                           
-                            <select
-                                onChange={(e) => OnInputChange(e)}
-                                class="form-control form-control-sm"
-                                name="user_type"
-                            >
-                                <option value={"admin"}>admin</option>
-                                <option value={"employee"}>employee</option>
-                            </select>
-                        </div>
-
-                    </div>
-
-                    <button type="submit" class="btn btn-primary updatebtn">Add User</button>
-                </form>
-            </div>
-
         </div>
     )
 }
